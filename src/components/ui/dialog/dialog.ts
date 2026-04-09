9b421c3e32143acd32332bcc4ece8c5c0db1e023
kukuhpr21 <kukuhpr21@gmail.com>
2026-04-09 11:22:47 +0700

    init 23

--- src/components/ui/dialog/dialog.ts
+++
@@ -1,9 +0,0 @@
-export const openModal = (title: string, description: string) => {
-  // Pastikan kode ini hanya jalan di browser
-  if (typeof window !== 'undefined') {
-    const event = new CustomEvent('open-modal', {
-      detail: { title, description }
-    });
-    window.dispatchEvent(event);
-  }
-};
